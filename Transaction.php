cf0bc58cba20f16b10c07c2a855257d30e5037ba
Goeran Heinemann <goeran@karsdorf.net>
2020-09-12 13:15:12 +0200

    simpler handling

--- Transaction.php
+++ Transaction.php
@@ -140,6 +140,42 @@ class Transaction
         return $return;
     }
 
+    public function markAsSent($id, $pdo): bool
+    {
+        $sql = "UPDATE shop.transactions SET bearbeitet=1 WHERE id=?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(1, $id);
+        if (!$stmt->execute()) {
+            return false;
+        }
+        return true;
+    }
+
+    public function sendTransaction($transaction, $token, $url): string
+    {
+        $submit = array();
+        $submit['token'] = $token;
+        $submit['transactions'] = $transaction['transaction'];
+
+
+        //url-ify the data for the POST
+        $fields_string = http_build_query($submit);
+
+        //open connection
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+
+        //So that curl_exec returns the contents of the cURL; rather than echoing it
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        //execute post
+        return curl_exec($ch);
+    }
+
     /*
      * this function expects an sql statement designed for pdo prepared statements and the associated values in an array, in the correct order
      * this function can be run as many times as needed and will then combine sql statements into one large transaction
--- send.php
+++ send.php
@@ -7,37 +7,19 @@ include "../../config/config.php";
 include "../../config/db.php";
 include 'config.php';
 
-$transaction = new Transaction();
-$transactions = $transaction->getPendingTransactions($pdo);
+$tr = new Transaction();
+$transactions = $tr->getPendingTransactions($pdo);
+
+if (count($transactions) == 0){
+    echo "nothing to do\n";
+    die();
+}
 
 foreach ($transactions as $transaction) {
-    $submit = array();
-    $submit['token'] = $token;
-    $submit['transactions'] = $transaction['transaction'];
-
-
-    //url-ify the data for the POST
-    $fields_string = http_build_query($submit);
-
-    //open connection
-    $ch = curl_init();
-
-    //set the url, number of POST vars, POST data
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-
-    //So that curl_exec returns the contents of the cURL; rather than echoing it
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    //execute post
-    $result = curl_exec($ch);
+    $result = $tr->sendTransaction($transaction, $token, $url);
 
     if ($result == 'success') {
-        $sql = "UPDATE shop.transactions SET bearbeitet=1 WHERE id=?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(1, $transaction['id']);
-        $stmt->execute();
+        $tr->markAsSent($transaction['id'], $pdo);
     } else {
         echo $result . "\n";
     }
