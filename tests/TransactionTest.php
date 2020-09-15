0a29e13387db102900f3544dda8d69a6824a9613
Goeran Heinemann <goeran@karsdorf.net>
2020-09-15 09:22:12 +0200

    added phpdoc and phpunit

---
+++ tests/TransactionTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace goeranh\Transmit;
+
+use PHPUnit\Framework\TestCase;
+
+class TransactionTest extends TestCase
+{
+
+    public function testRunTransaction()
+    {
+
+    }
+
+    public function testGetLastInsertID()
+    {
+
+    }
+
+    public function testCommit()
+    {
+
+    }
+
+    public function testMarkAsSent()
+    {
+
+    }
+
+    public function testAddTransation()
+    {
+
+    }
+
+    public function testSubstituteDatabaseName()
+    {
+
+    }
+
+    public function testCreateTransactionJSON()
+    {
+
+    }
+
+    public function testGetPendingTransactions()
+    {
+
+    }
+
+    public function testGetErrorCode()
+    {
+
+    }
+
+    public function testGetErrorMessages()
+    {
+
+    }
+
+    public function testSendTransaction()
+    {
+
+    }
+}
