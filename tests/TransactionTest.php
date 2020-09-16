2f4c8e330245fb46a96f021133468b2d53880bd0
Goeran Heinemann <goeran@karsdorf.net>
2020-09-16 15:27:01 +0200

    removed testcases

--- tests/TransactionTest.php
+++ tests/TransactionTest.php
@@ -1,65 +1,35 @@
 <?php
-
 namespace goeranh\Transmit;
 
 use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertNotEquals;
 
 class TransactionTest extends TestCase
 {
-    //todo implement phpunit tests
-
-    public function testRunTransaction()
-    {
-
-    }
-
-    public function testGetLastInsertID()
-    {
-
-    }
-
-    public function testCommit()
-    {
-
-    }
-
-    public function testMarkAsSent()
-    {
-
-    }
-
     public function testAddTransaction()
     {
-
-    }
-
-    public function testSubstituteDatabaseName()
-    {
-
+        $transaction = new Transaction(null);
+        $sql = "UPDATE shop.`produkte` SET `bezeichnung` = ? and hersteller = ? WHERE `produkte`.`barcode` = ?;";
+        $transaction->addTransaction($sql, array('', ''), array('barcode'), array(1));
+        //todo test insert
     }
 
     public function testCreateTransactionJSON()
     {
-
-    }
-
-    public function testGetPendingTransactions()
-    {
-
-    }
-
-    public function testGetErrorCode()
-    {
-
+        $transaction = new Transaction(null);
+        $sql = "UPDATE shop.`produkte` SET `bezeichnung` = ? and hersteller = ? WHERE `produkte`.`barcode` = ?;";
+        $transaction->addTransaction($sql, array('', ''), array('barcode'), array(1));
+        $test = $transaction->createTransactionJSON();
+        $this->assertEquals('[{"action":"update","data":{"database-name":"shop","table-name":"produkte ","fields":["bezeichnung","hersteller"],"values":["",""],"where":{"fields":["barcode"],"values":[1]}}}]', $transaction->createTransactionJSON());
     }
 
     public function testGetErrorMessages()
     {
-
-    }
-
-    public function testSendTransaction()
-    {
-
+        $transaction = new Transaction(null);
+        $sql = "UPDATE shop.`produkte` SET `bezeichnung` = ? and hersteller = ? WHERE `produkte`.`barcode` = ?;";
+        $this->expectException("The number of fields for your where clause does not match the number of values you gave");
+        $transaction->addTransaction($sql, array('', ''));
+        self::assertEquals($transaction->getErrorCode(), 1);
+        assertNotEquals($transaction->getErrorMessages(), '');
     }
 }
